f5b56ac0a75f9f0ad0c5fa08adc7724745395aea
Dominick Leppich <dominick.leppich@intranda.com>
2025-03-18 12:10:03 +0100

    wip: fix bash url path

--- module-api/src/main/java/org/goobi/api/rest/SizeValidationPluginApi.java
+++ module-api/src/main/java/org/goobi/api/rest/SizeValidationPluginApi.java
@@ -6,8 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Request;
+import jakarta.ws.rs.core.UriInfo;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang.StringUtils;
 import org.goobi.beans.Process;
@@ -25,12 +27,12 @@ import java.util.stream.Stream;
 public class SizeValidationPluginApi {
     @GET
     @Path("/process/{processid}/media/list")
-    public List<URI> mediaList(@PathParam("processid") int processid, Request request) throws SwapException, IOException {
+    public List<URI> mediaList(@PathParam("processid") int processid, @Context UriInfo uriInfo) throws SwapException, IOException {
         Process p = ProcessManager.getProcessById(processid);
         java.nio.file.Path mediaFolder = Paths.get(p.getImagesTifDirectory(false));
 
 //        String baseUrl = request.getMethod().substring(0, request.getRequestURI().indexOf("plugins/")) + "api/image/" + p.getId() + "/media/";
-        String baseUrl = "/test/";
+        String baseUrl = uriInfo.getPath().substring(0, uriInfo.getPath().indexOf("plugins/")) + "api/image/" + p.getId() + "/media/";
 
         try (Stream<java.nio.file.Path> imagePaths = Files.list(mediaFolder)) {
             List<URI> images = imagePaths.map(path -> URI.create(baseUrl + path.getFileName().toString()))
